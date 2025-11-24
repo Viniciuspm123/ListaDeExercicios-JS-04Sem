99f4437b08b0c5f0812ea0ea9255fafa3a4a68b9
Vinicius Marques <160979244+Viniciuspm123@users.noreply.github.com>
2025-11-24 16:41:26 -0300

    Delete Lista2 directory

--- Lista2/js/script01.js
+++
@@ -1,9 +0,0 @@
-function idade() {
-    let idade = parseInt(prompt("Digite a sua idade em anos :"))
-    if (idade < 18) {
-        alert("É menor de idade")
-    } else {
-        alert("É maior de idade")
-    }
-
-}
